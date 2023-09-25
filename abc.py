3e4f456add9d7814f211d99bc2eaac1d9ac0085a
Harsh Raj <harshraj@Harshs-MacBook-Pro-4.local>
2023-09-25 14:42:39 +0530

    Add

---
+++ abc.py
@@ -0,0 +1,4 @@
+Python 3.10.11 (v3.10.11:7d4cc5aa85, Apr  4 2023, 19:05:19) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello World")
+Hello World
